fbdecc8b89fcb238172870f5839ef926877503bd
vineetyadav2403 <46716473+vineetyadav2403@users.noreply.github.com>
2024-03-18 19:23:58 +0000

    floyd:

--- Graph/Floyd.java
+++ Graph/Floyd.java
@@ -1,3 +1,47 @@
-public class Floyd {
-    
+class FloydWarshallShortestPath {
+    static final int INF = 99999; // Represents infinity
+
+    static void floydWarshall(int graph[][], int V) {
+        int dist[][] = new int[V][V];
+
+        // Initialize the distance array with the values from the graph
+        for (int i = 0; i < V; i++)
+            for (int j = 0; j < V; j++)
+                dist[i][j] = graph[i][j];
+
+        // Apply the Floyd-Warshall algorithm
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    // If vertex k is on the shortest path from i to j,
+                    // then update the value of dist[i][j]
+                    if (dist[i][k] + dist[k][j] < dist[i][j])
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+
+        // Print the shortest distances between every pair of vertices
+        System.out.println("Shortest distances between every pair of vertices:");
+        for (int i = 0; i < V; ++i) {
+            for (int j = 0; j < V; ++j) {
+                if (dist[i][j] == INF)
+                    System.out.print("INF\t");
+                else
+                    System.out.print(dist[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int graph[][] = {
+                {0, 5, INF, 10},
+                {INF, 0, 3, INF},
+                {INF, INF, 0, 1},
+                {INF, INF, INF, 0}
+        };
+        int V = 4;
+        floydWarshall(graph, V);
+    }
 }
